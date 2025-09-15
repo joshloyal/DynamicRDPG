874364b9a949521cd5a52e056ca2fb949d8bc816
Joshua Loyal <joshua.d.loyal@gmail.com>
2025-09-14 21:22:02 -0400

    removed old imports

--- dynrdpg/datasets/__init__.py
+++ dynrdpg/datasets/__init__.py
@@ -1,3 +1,2 @@
-from .synthetic import simulate_network_rw, simulate_network_gp, simulate_network_ncr, simulate_network_bspline
-from .load_news import *
+from .synthetic import simulate_network_gp, simulate_network_bspline
 from .load_polecat import *
