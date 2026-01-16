04869fbb855bcfa71a73702817cca0501dea0e3b
Joshua Loyal <joshua.d.loyal@gmail.com>
2026-01-16 12:47:58 -0500

    updated init

--- dynrdpg/datasets/__init__.py
+++ dynrdpg/datasets/__init__.py
@@ -1,2 +1,2 @@
-from .synthetic import simulate_network_gp, simulate_network_bspline
+from .synthetic import simulate_network_gp, simulate_network_bspline, simulate_network_gp_continuous
 from .load_polecat import *
