e22a02abd13d05bcde7b53819981af5fc31854de
Joshua Loyal <joshua.d.loyal@gmail.com>
2026-04-05 13:25:29 -0400

    added new simulation for varying density

--- dynrdpg/datasets/synthetic.py
+++ dynrdpg/datasets/synthetic.py
@@ -108,6 +108,50 @@ def simulate_network_gp(n_nodes=100, n_time_steps=100, n_features=2,
     return Ys, X, np.stack(probas)
 
 
+def simulate_network_gp_density(n_nodes=100, n_time_steps=100, n_features=2, 
+                        length_scale=3, gp_type='matern', nu=2.5,
+                        density_min=0.1, density_max=0.5, random_state=42):
+    rng = check_random_state(random_state)
+    ts = np.arange(n_time_steps).reshape(-1, 1)
+    dist_sq = euclidean_distances(ts, squared=True)
+    
+    if gp_type == 'rbf':
+        cov = 5 * RBF(length_scale=n_time_steps/length_scale)(ts)
+    else:
+        cov = 5 * Matern(length_scale=n_time_steps/length_scale, nu=nu)(ts)
+    
+    X = rng.multivariate_normal(np.zeros_like(ts.ravel()), cov=cov,
+                                size=(n_nodes, n_features)).transpose((2, 0, 1))
+    X = expit(X) / np.sqrt(n_features)
+    
+    def density(t):
+        return (density_max - density_min) * np.sin(np.pi * t / n_time_steps) + density_min 
+
+    means = []
+    subdiag = np.tril_indices(n_nodes, k=-1)
+    scales = []
+    for t in range(n_time_steps):
+        means.append((X[t] @ X[t].T)[subdiag])
+        scales.append(density(t) / np.mean(means[t]))
+
+    Ys = []
+    probas = []
+    for t in range(n_time_steps):
+        Y = np.zeros((n_nodes, n_nodes))
+
+        X[t] *= np.sqrt(scales[t])
+        y = rng.binomial(1, np.clip(scales[t] * means[t], 0, 1))
+        probas.append(np.clip(X[t] @ X[t].T, 0, 1)[subdiag])
+
+        Y[subdiag] = y
+        Y += Y.T
+
+        Y = sp.csr_array(Y, dtype=float) 
+        Ys.append(Y)
+
+    return Ys, X, np.stack(probas)
+
+
 def simulate_network_gp_mixture(n_nodes=100, n_time_steps=100, n_features=2, 
                         length_scale=3, gp_type='matern', nu=2.5, 
                         density=0.2, random_state=42):
@@ -122,6 +166,57 @@ def simulate_network_gp_mixture(n_nodes=100, n_time_steps=100, n_features=2,
     
     X = rng.multivariate_normal(np.zeros_like(ts.ravel()), cov=cov,
                                 size=(n_nodes, n_features)).transpose((2, 0, 1))
+    if n_features == 2:
+        z = rng.choice([0, 1, 2], size=n_nodes)
+        alpha = np.array([[1, 1, 10],
+                          [1, 10, 1],
+                          [10, 1, 1]])
+        X0 = logit(Dirichlet(alpha[z]).sample(PRNGKey(random_state))[:, :2]) 
+    else:
+        z = rng.choice([0, 1, 2, 3, 4], size=n_nodes)
+        alpha = np.array([[1, 1, 1, 1,10],
+                          [1, 1, 1, 10, 1],
+                          [1, 1, 10, 1, 1],
+                          [1, 10, 1, 1, 1],
+                          [10, 1, 1, 1, 1]])
+        X0 = logit(Dirichlet(alpha[z]).sample(PRNGKey(random_state))[:, :4]) 
+    X = expit(X0 + X) / np.sqrt(n_features)
+
+    means = []
+    subdiag = np.tril_indices(n_nodes, k=-1)
+    for t in range(n_time_steps):
+        means.append((X[t] @ X[t].T)[subdiag])
+    
+    scale = density / np.mean(means)
+
+    Ys = []
+    probas = []
+    for t in range(n_time_steps):
+        Y = np.zeros((n_nodes, n_nodes))
+
+        X[t] *= np.sqrt(scale)
+        y = rng.binomial(1, np.clip(scale * means[t], 0, 1))
+        probas.append(np.clip(X[t] @ X[t].T, 0, 1)[subdiag])
+
+        Y[subdiag] = y
+        Y += Y.T
+
+        Y = sp.csr_array(Y, dtype=float) 
+        Ys.append(Y)
+
+    return Ys, X, np.stack(probas), z
+
+
+def simulate_network_mixture_rw(n_nodes=100, n_time_steps=100, n_features=2, 
+                        rho=0.95, sigma=5, density=0.2, random_state=42):
+    rng = check_random_state(random_state)
+    
+    X = np.zeros((n_time_steps, n_nodes, n_features)) 
+    X[0] = sigma * rng.randn(n_nodes, n_features) 
+    rw_sigma_sq = (sigma ** 2)  * (1. - rho ** 2)
+    for t in range(1, n_time_steps):
+        X[t] = rho * X[t-1] +  np.sqrt(rw_sigma_sq) * rng.randn(n_nodes, n_features)
+     
     if n_features == 2:
         z = rng.choice([0, 1, 2], size=n_nodes)
         alpha = np.array([[1, 1, 10],
